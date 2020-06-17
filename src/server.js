ada5886c52438b53af649ce241a70a782b1461e6
kureharyosuke <ohhayo.com@gmail.com>
2020-06-18 00:55:22 +0900

    #2.1 Hello world part Two

--- src/server.js
+++ src/server.js
@@ -2,26 +2,19 @@ import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
 import logger from "morgan";
-
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
-app.use(express.static(join(__dirname, "static")));
 app.use(logger("dev"));
+app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
-
 const handleListening = () =>
   console.log(`✅ Server running: http://localhost:${PORT}`);
-
 const server = app.listen(PORT, handleListening);
 
 const io = socketIO.listen(server);
 
-let sockets = [];
-
 io.on("connection", (socket) => {
-  sockets.push(socket.id);
+  socket.on("helloGuys", () => console.log("the client said hello"));
 });
-
-setInterval(() => console.log(sockets), 1000);
