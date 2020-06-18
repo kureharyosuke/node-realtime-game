b1ff09291f00d062d5362897791c7d96e0deebc7
kureharyosuke <ohhayo.com@gmail.com>
2020-06-18 18:27:14 +0900

    #2.2 Recap

--- src/server.js
+++ src/server.js
@@ -2,13 +2,16 @@ import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
 import logger from "morgan";
+
 const PORT = 4000;
 const app = express();
+
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
+
 const handleListening = () =>
   console.log(`✅ Server running: http://localhost:${PORT}`);
 const server = app.listen(PORT, handleListening);
