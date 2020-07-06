70c16ee7ed8ee18bfabf408289c3025670d1d6f0
kureharyosuke <ohhayo.com@gmail.com>
2020-07-06 17:03:28 +0900

    #4.2 Global Variables

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,15 +1,14 @@
 const body = document.querySelector("body");
 const loginForm = document.getElementById("jsLogin");
-
 const NICKNAME = "nickname";
 const LOGGED_OUT = "loggedOut";
 const LOGGED_IN = "loggedIn";
-
 const nickname = localStorage.getItem(NICKNAME);
 
 const logIn = (nickname) => {
-  const socket = io("/");
-  socket.emit("setNickname", { nickname });
+  // eslint-disable-next-line no-undef
+  window.socket = io("/");
+  window.socket.emit(window.events.setNickname, { nickname });
 };
 
 if (nickname === null) {
@@ -18,7 +17,6 @@ if (nickname === null) {
   body.className = LOGGED_IN;
   logIn(nickname);
 }
-
 const handleFormSubmit = (e) => {
   e.preventDefault();
   const input = loginForm.querySelector("input");
@@ -28,7 +26,6 @@ const handleFormSubmit = (e) => {
   body.className = LOGGED_IN;
   logIn(value);
 };
-
 if (loginForm) {
   loginForm.addEventListener("submit", handleFormSubmit);
 }
--- src/server.js
+++ src/server.js
@@ -3,20 +3,20 @@ import express from "express";
 import socketIO from "socket.io";
 import logger from "morgan";
 import socketController from "./socketController";
+import events from "./events";
 
 const PORT = 4000;
 const app = express();
-
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (req, res) =>
+  res.render("home", { events: JSON.stringify(events) })
+);
 
 const handleListening = () =>
   console.log(`✅ Server running: http://localhost:${PORT}`);
 const server = app.listen(PORT, handleListening);
-
 const io = socketIO.listen(server);
-
 io.on("connection", (socket) => socketController(socket));
